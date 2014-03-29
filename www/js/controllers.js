f0d20252e3676febf1527b22f3d14d3bfda6502d
Bjarte Johansen <bjarte.johansen@gmail.com>
2014-03-29 16:57:52 +0100

    working camera

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,7 +4,23 @@ angular.module('starter.controllers', [])
 })
 
 .controller('HomeCtrl', function($scope) {
-
+    $scope.takePic = function() {
+        var options =   {
+            quality: 50,
+            destinationType: Camera.DestinationType.FILE_URI,
+            sourceType: 1,      // 0:Photo Library, 1=Camera, 2=Saved Photo Album
+            encodingType: 0     // 0=JPG 1=PNG
+        }
+        navigator.camera.getPicture(onSuccess,onFail,options);
+    }
+    var onSuccess = function(FILE_URI) {
+        console.log(FILE_URI);
+        $scope.picData = FILE_URI;
+        $scope.$apply();
+    };
+    var onFail = function(e) {
+        console.log("On fail " + e);
+    }
 
 })
 
